cbe9c2334e164a01d645de5d483cb68f04d3bcc5
mohdshuhaib <mohdshuhaib357@gmail.com>
2024-12-22 07:21:01 +0530

    added lg and sm datas sample

--- components/LargeData.tsx
+++ components/LargeData.tsx
@@ -2,7 +2,7 @@
 const markerbig: Marker[] = [
   {
     id: 1,
-    title: "Store",
+    title: "Stage - 1",
     description: "This is the campus store where you can buy essentials.",
     image: "/store.jpg",
     position: { top: "46%", left: "91%" },
@@ -10,15 +10,151 @@ const markerbig: Marker[] = [
   },
   {
     id: 2,
-    title: "Hostel Block",
+    title: "Stage - 2",
     description: "The hostel block for students.",
     image: "/hostel.jpg",
     position: { top: "65%", left: "52%" },
     hostel: 'mlar'
   },
   {
+    id: 3,
+    title: "Stage - 3",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 4,
+    title: "Stage - 4",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 5,
+    title: "Stage - 5",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 6,
+    title: "Stage - 6",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 7,
+    title: "Reception",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 8,
+    title: "Fest Office",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 9,
+    title: "Clok Room",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
     id: 10,
+    title: "Mini Expo",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 11,
+    title: "Main entrance",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 12,
+    title: "PMSA Masjid",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 13,
+    title: "PMSA Main office",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 14,
+    title: "Parking",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 15,
     title: "Canteen",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 16,
+    title: "Toilet & Shower",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 17,
+    title: "Store",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 18,
+    title: "Hostel",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 19,
+    title: "PMSA Wafy college",
+    description: "Enjoy a variety of meals and snacks here.",
+    image: "/canteen.jpg",
+    position: { top: "26%", left: "69%" },
+    hostel: 'piyush'
+  },
+  {
+    id: 20,
+    title: "PMSA Hospital",
     description: "Enjoy a variety of meals and snacks here.",
     image: "/canteen.jpg",
     position: { top: "26%", left: "69%" },
